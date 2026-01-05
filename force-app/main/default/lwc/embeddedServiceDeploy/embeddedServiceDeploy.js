2b94a397c77609382889c0a8dea972d3a2883219
Jay Kariesch <kariesch@google.com>
2026-01-05 15:34:23 +0000

    refactor(embeddedServiceDeploy): removes handlers from js file

--- force-app/main/default/lwc/embeddedServiceDeploy/embeddedServiceDeploy.js
+++ force-app/main/default/lwc/embeddedServiceDeploy/embeddedServiceDeploy.js
@@ -1,14 +1,4 @@
 // @ts-check
 import { LightningElement } from "lwc";
 
-export default class Wizard extends LightningElement {
-    selectedTab = "1";
-
-    onNext() {
-        this.selectedTab = (parseInt(this.selectedTab, 10) + 1).toString();
-    }
-
-    onSelect(ev) {
-        console.log('ON SELECT', ev)
-    }
-}
+export default class EmbeddedServiceDeploy extends LightningElement {}
